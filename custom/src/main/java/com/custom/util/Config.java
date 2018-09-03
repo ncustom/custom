d719a24521f0388c73b8b577f067f14ddd7517da
dhf1 <13042651804@163.com>
2018-09-03 19:46:14 +0800

    项目搭建

---
+++ custom/src/main/java/com/custom/util/Config.java
@@ -0,0 +1,27 @@
+package com.custom.util;
+
+/**
+ * 系统常量
+ */
+public class Config
+{
+	/**
+	 * url前半部分
+	 */
+	public static final String BASE_URL = "https://api.miaodiyun.com/20150822";
+
+	/**
+	 * 开发者注册后系统自动生成的账号，可在官网登录后查看
+	 */
+	public static final String ACCOUNT_SID = "0dfcf7f230d54e0880b80e3016193026";
+
+	/**
+	 * 开发者注册后系统自动生成的TOKEN，可在官网登录后查看
+	 */
+	public static final String AUTH_TOKEN = "e5ac90b7182646a79f919ef46569028c";
+
+	/**
+	 * 响应数据类型, JSON或XML
+	 */
+	public static final String RESP_DATA_TYPE = "json";
+}
