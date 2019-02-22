453f70113f1b09da4dc9aa933dea45d1ff36d787
DESKTOP-0N4KRRI\dhf <duhf@asiainfo.com>
2019-02-22 22:30:56 +0800

    增加任务分页，删除根目录下res文件夹，删除根目录下index.jsp

--- custom/src/main/java/com/dhf/service/impl/TaskServiceImpl.java
+++ custom/src/main/java/com/dhf/service/impl/TaskServiceImpl.java
@@ -1,10 +1,12 @@
 package com.dhf.service.impl;
 
+import com.dhf.domain.PageBean;
 import com.dhf.mapper.TaskMapper;
 import com.dhf.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,9 +16,29 @@ public class TaskServiceImpl implements TaskService {
     @Autowired
     private TaskMapper taskMapper;
 
-
     @Override
-    public List<Map<String, Object>> selectTasksByCode(String code) {
-        return taskMapper.selectTasksByCode(code);
+    public PageBean<Map<String, Object>> selectTasksByCodeByPage(String code, Integer currPage) {
+        PageBean<Map<String, Object>> pageBean = new PageBean();
+        //封装当前页数
+        pageBean.setCurrPage(currPage);
+        //封装每页显示的记录数
+        int pageSize = 15;
+        pageBean.setPageSize(pageSize);
+        //封装总记录数
+        int totalCount = taskMapper.selectCountsByCode(code);
+        pageBean.setTotalCount(totalCount);
+        //封装总页数
+        double tc = totalCount;
+        Double num = Math.ceil(tc/pageSize);
+        pageBean.setTotalPage(num.intValue());
+        //封装每页显示的数据
+        int begin = (currPage-1)*pageSize;
+        Map map = new HashMap();
+        map.put("code", code);
+        map.put("begin", begin);
+        map.put("pageSize", pageSize);
+        List<Map<String, Object>> tasks = taskMapper.selectTasksByCodeAndPage(map);
+        pageBean.setList(tasks);
+        return pageBean;
     }
 }
